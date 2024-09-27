32e147577a32cd3872b7a7daaf5e0e13d5f2f0ba
Maaanav <113304883+Maaanav@users.noreply.github.com>
2024-09-27 11:46:23 +0000

    Sync LeetCode submission Runtime - 1 ms (21.40%), Memory - 41.2 MB (99.89%)

---
+++ 0216-combination-sum-iii/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(), k, n, 1);
+        return res; 
+    }
+
+    public void backtrack(List<List<Integer>> res,List<Integer> temp, int k, int rem, int start){
+        if(temp.size() == k && rem == 0){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        if(temp.size() > k || rem < 0){
+            return;
+        }
+
+        for(int i =start; i<=9; i++){
+            temp.add(i);
+            backtrack(res, temp, k, rem-i, i+1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}

f676fb80f0960148685088a8cfb11284fd6f10ee
Maaanav <113304883+Maaanav@users.noreply.github.com>
2026-06-04 16:37:57 +0000

    Sync LeetCode submission Runtime - 35 ms (71.13%), Memory - 22 MB (78.61%)

---
+++ 0853-most-profit-assigning-work/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        jobs = sorted(zip(difficulty, profit))
+        worker.sort()
+
+        total_profit = 0
+        max_profit = 0
+        i = 0
+        n = len(jobs)
+
+        for a in worker:
+            while i < n and jobs[i][0] <= a:
+                max_profit = max(max_profit, jobs[i][1])
+                i += 1
+            
+            total_profit += max_profit
+        
+        return total_profit

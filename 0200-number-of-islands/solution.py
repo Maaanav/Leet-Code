51d8aa89c2c52a5d2b3c02f63d3e679119fda9a3
Maaanav <113304883+Maaanav@users.noreply.github.com>
2026-08-11 18:04:14 +0000

    Sync LeetCode submission Runtime - 227 ms (92.25%), Memory - 21.6 MB (48.39%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -3,27 +3,27 @@ class Solution:
         if not grid or not grid[0]:
             return 0
         
-        rows,cols = len(grid), len(grid[0])
-        nums_island = 0
+        rows, cols = len(grid), len(grid[0])
+        island = 0
 
-        def dfs(r,c):
-            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == '0':
+        def dfs(r: int, c: int) -> None:
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
                 return
 
             grid[r][c] = '0'
 
-            dfs(r+1,c)
-            dfs(r,c+1)
+            dfs(r+1,c) 
             dfs(r-1,c)
+            dfs(r,c+1)
             dfs(r,c-1)
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
-                    nums_island +=1
+                    island += 1
                     dfs(r,c)
-                
-        return nums_island
 
+        return island
+         
 
 
